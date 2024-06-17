2c5b98ca2ce487542f2cbefb7a648b01e8c77888
David-Okello <okellodavid002@gmail.com>
2024-06-17 16:55:40 +0300

    Fix deployment script and settings

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project ..."
+# Ensure pip is installed
+python3.9 -m ensurepip --upgrade
+
+# Install Python dependencies
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
-echo "Make Migration ..."
+# Run Django migrations
+echo "Making Migrations ..."
 python3.9 manage.py makemigrations --noinput
+echo "Applying Migrations ..."
 python3.9 manage.py migrate --noinput
 
-echo "Collect Static ..."
+# Collect static files
+echo "Collecting Static Files ..."
 python3.9 manage.py collectstatic --noinput --clear
