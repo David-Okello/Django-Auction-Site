994aa6829a7fb9ba4e296991090f9792823ced07
David-Okello <okellodavid002@gmail.com>
2024-06-22 19:55:51 +0300

    Modified settings

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,6 @@ dbHost = os.getenv('dbHost')
 dbPort = os.getenv('dbPort')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -99,7 +98,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 AUTH_USER_MODEL = 'auctions.User'
 
@@ -140,7 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
